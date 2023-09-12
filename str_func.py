9059e83e7bf40b3085423fb5972774aa77dd15c2
Dmitri Sementsov <pluribus.zuhi@gmail.com>
2023-09-12 18:29:55 +0700

    add function raise_first_letter add docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def raise_register(word):
+    """увеличивает регистр всех букв"""
     return word.upper()
+
+
+def raise_first_letter(string_):
+    """увеличивает регистр первой буквы каждого слова"""
+    string_list = string_.split()
+    result_string = ' '.join(string_list).title()
+
+    return result_string
